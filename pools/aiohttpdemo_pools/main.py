4c2b741b3b51c1ca32a1f8b025a903cd45000018
Contemplat0r <uldo.forme@gmail.com>
2020-12-07 19:28:12 +0200

    Now app object not use for transfer Rabbitmq connection data (connection, channel, exchange) between various parts of application. Object of special class PikaConnectionProxy used now for it.

--- pools/aiohttpdemo_pools/main.py
+++ pools/aiohttpdemo_pools/main.py
@@ -11,25 +11,49 @@ from typing import Dict
 #from bson import ObjectId, json_util
 from routes import setup_routes
 
-async def setup_rabbitmq(app):
+class PikaConnectionProxy:
+
+    def __init__(
+            self,
+            connection=None,
+            channel=None,
+            exchange=None
+            ):
+        self.connection = connection
+        self.channel = channel
+        self.exchange = exchange
+
+pika_connection_proxy = PikaConnectionProxy()
+
+#async def setup_rabbitmq(app):
+async def setup_rabbitmq(pika_connection_proxy):
     try:
         connection: aio_pika.Connection = await aio_pika.connect_robust('amqp://guest:guest@127.0.0.1', loop=loop)
-        app['connection'] = connection
+        #app['connection'] = connection
+        pika_connection_proxy.connection = connection
     except (ConnectionError, aiormq.exceptions.IncompatibleProtocolError) as e:
         print(e)
 
         await asyncio.sleep(10)
+        await setup_rabbitmq(pika_connection_proxy)
         return None
 
     channel = await connection.channel()
     exchange = channel.default_exchange
     print(f"action=setup_rabbitmq, status=success")
-    app['exchange'] = exchange
-    setup_routes(app)
+    #app['exchange'] = exchange
+    pika_connection_proxy.channel = channel
+    pika_connection_proxy.exchange = exchange
+    #setup_routes(app)
+    setup_routes(app, pika_connection_proxy)
 
-async def close_rabbitmq(app):
-    if app.get('connection'):
-        await app['connection'].close()
+#async def close_rabbitmq(app):
+#    if app.get('connection'):
+#        await app['connection'].close()
+
+async def close_rabbitmq(pika_connection_proxy):
+    if pika_connection_proxy.connection:
+        await pika_connection_proxy.connection.close()
 
 
 loop = asyncio.get_event_loop()
--- pools/aiohttpdemo_pools/routes.py
+++ pools/aiohttpdemo_pools/routes.py
@@ -1,13 +1,10 @@
-#from views import index
 from functools import partial
 
 import views
 
-#def setup_routes(app, loop):
-#    index = partial(views.generate_index, loop=loop)
-#    app.router.add_get('/', index)
-
-def setup_routes(app):
+#def setup_routes(app):
+def setup_routes(app, pika_connection_proxy):
     print("setup_routes")
-    index = partial(views.generate_index, exchange=app['exchange'])
+    #index = partial(views.generate_index, exchange=app['exchange'])
+    index = partial(views.generate_index, exchange=pika_connection_proxy.exchange)
     app.router.add_get('/', index)
